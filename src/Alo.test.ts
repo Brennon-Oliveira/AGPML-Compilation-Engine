55bd96638b0f2c7a6f52cf0e57cbd3cdb3f7e50e
BrennonOliveira <brennonoliveira20014@gmail.com>
2022-12-20 03:07:45 -0300

    Adicionando agpml example

--- src/Alo.test.ts
+++
@@ -1,7 +0,0 @@
-import { Alo } from "./Alo"
-
-describe("Alo", ()=>{
-    it("Aii", ()=>{
-        expect(Alo()).toEqual(1)
-    })
-})
--- src/Alo.ts
+++
@@ -1,3 +0,0 @@
-export function Alo(): number{
-    return 1;
-}
