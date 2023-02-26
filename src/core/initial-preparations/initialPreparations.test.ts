ecd5a8e4a20bfa586b2adc50a599bc31705c37cf
brennon.oliveira@docato.com.br <brennon.oliveira@docato.com.br>
2023-02-26 16:24:05 -0300

    Removendo requisios de typescript

--- src/core/initial-preparations/initialPreparations.test.ts
+++
@@ -1,9 +0,0 @@
-
-describe("Initial Preparations", ()=>{
-
-
-    it("Should separe Header and Body", ()=>{
-        expect(1).toEqual(1)
-    })
-
-})
--- src/core/initial-preparations/initialPreparations.ts
+++
@@ -1,6 +0,0 @@
-export const separeHeaderAndBody = (content: string): [string, string] | Error =>{
-    
-    
-
-    throw new Error("Erro");
-}
